93875e8367770359b282436c4b891c4ceafac256
staikone <36898607+staikone@users.noreply.github.com>
2025-09-27 07:24:24 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,7 @@ window.addEventListener("scroll", scrollUp);
 /*==================== DARK LIGHT THEME ====================*/
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
+const iconTheme = "uil-moon"; // промяна от uil-sun на uil-moon
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
@@ -180,25 +180,23 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
     document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+    themeButton.classList.contains(iconTheme) ? "uil-sun" : "uil-moon";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
         darkTheme
     );
-    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    themeButton.classList[selectedIcon === "uil-sun" ? "add" : "remove"](
         iconTheme
     );
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme);
     themeButton.classList.toggle(iconTheme);
-    // We save the theme and the current icon that the user chose
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
